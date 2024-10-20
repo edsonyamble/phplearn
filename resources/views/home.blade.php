3d05625c13cdb59ffd3bc63825cf55406cfcac3a
Edson Yamble <edsonyamble2@gmail.com>
2024-10-20 14:52:27 +0700

    add logo image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Shop</title>
 </head>
 <body>
-123453
+<div class="top-line">
+    <div class="container">
+        <div class="top-line__main">
+            <div class="select-city">
+                <span class="select-city__text">Mockva</span>
+                <div class="select-city__icon"></div>
+            </div>
+            <div class="pick-up-point">
+                <span class="pick-up-point__text">Пункт выдачи</span>
+                <div class="pick-up-point__icon"></div>
+            </div>
+            <span class="top-line-time">Пн-Пт: <strong>C 9:00 - 17:00</strong></span>
+        </div>
+        <div class="user-account">
+            <div class="user-account__icon"></div>
+            <span class="user-account__text">Личный кабинет</span>
+        </div>
+    </div>
+</div>
+<div class="midle-line">
+    <div class="container">
+        <a href="" class="logo">
+            <img src="{{asset('images/logo.png')}}" alt="">
+        </a>
+    </div>
+
+
+</div>
 </body>
 </html>
