7ca42e68904d182939b6bb9e353cc70e7c450369
Edson Yamble <edsonyamble2@gmail.com>
2024-10-20 17:07:09 +0700

    add midle line

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,25 +12,42 @@
         <div class="top-line__main">
             <div class="select-city">
                 <span class="select-city__text">Mockva</span>
-                <div class="select-city__icon"></div>
+                <div class="select-city__icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                </div>
             </div>
             <div class="pick-up-point">
                 <span class="pick-up-point__text">Пункт выдачи</span>
-                <div class="pick-up-point__icon"></div>
+                <div class="pick-up-point__icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                </div>
             </div>
             <span class="top-line-time">Пн-Пт: <strong>C 9:00 - 17:00</strong></span>
         </div>
         <div class="user-account">
-            <div class="user-account__icon"></div>
+            <div class="user-account__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z"/></svg></div>
             <span class="user-account__text">Личный кабинет</span>
         </div>
     </div>
 </div>
 <div class="midle-line">
-    <div class="container">
+    <div class="container">x
         <a href="" class="logo">
             <img src="{{asset('images/logo.png')}}" alt="">
         </a>
+        <form action="" class="fast-search">
+            <div class="fast-search__input">
+                <input type="text" name="v" placeholder="Поиск" class="fast-search__input">
+                <div class="fast-search__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></div>
+            </div>
+            <span class="fast-search__example">
+                например: кроссовки, футболка
+            </span>
+        </form>
+        <div class="ml-callback">
+            <a class="ml-callback__phone" tel="88005553535" href="">8 (800) 555-35-35</a>
+            <a href="#" class="ml-callback__call">Заказать звонок</a>
+        </div>
     </div>
 
 
