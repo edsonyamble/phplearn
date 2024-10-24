962ee844703dce7e6dc1817c9c1198d2527f0746
Edson Yamble <edsonyamble2@gmail.com>
2024-10-24 17:02:45 +0700

    css add to second block

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,41 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Shop</title>
 </head>
 <body>
 <div class="top-line">
-    <div class="container">
-        <div class="top-line__main">
-            <div class="select-city">
+    <div class="container flex-center">
+        <div class="top-line__main flex-center">
+            <div class="select-city flex-center">
                 <span class="select-city__text">Mockva</span>
                 <div class="select-city__icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+                    <img src="{{asset('images/select.svg')}}" alt="">
                 </div>
             </div>
-            <div class="pick-up-point">
-                <span class="pick-up-point__text">Пункт выдачи</span>
+            <div class="pick-up-point flex-center">
                 <div class="pick-up-point__icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                    <img src="{{asset('images/local.svg')}}" alt="">
                 </div>
+                <span class="pick-up-point__text">Пункт выдачи</span>
             </div>
-            <span class="top-line-time">Пн-Пт: <strong>C 9:00 - 17:00</strong></span>
+            <div class="top-line-time flex-center"> <span class="top-line-time__text"> Пн-Пт: <span>C 9:00 - 17:00</span></span></div>
         </div>
-        <div class="user-account">
-            <div class="user-account__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z"/></svg></div>
+        <div class="user-account flex-center">
+            <div class="user-account__icon">
+                <img src="{{asset('images/acount.svg')}}" alt="">
+            </div>
             <span class="user-account__text">Личный кабинет</span>
         </div>
     </div>
 </div>
 <div class="midle-line">
-    <div class="container">x
-        <a href="" class="logo">
+    <div class="container flex-center">
+        <a href="" class="ml-logo">
             <img src="{{asset('images/logo.png')}}" alt="">
         </a>
         <form action="" class="fast-search">
             <div class="fast-search__input">
                 <input type="text" name="v" placeholder="Поиск" class="fast-search__input">
-                <div class="fast-search__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></div>
+                <div class="fast-search__icon">
+                    <img src="{{asset('images/search.svg')}}" alt="">
+                </div>
             </div>
             <span class="fast-search__example">
                 например: кроссовки, футболка
@@ -48,9 +53,79 @@
             <a class="ml-callback__phone" tel="88005553535" href="">8 (800) 555-35-35</a>
             <a href="#" class="ml-callback__call">Заказать звонок</a>
         </div>
+        <div class="ml-action flex-center">
+            <div class="ml-action__compare">
+                <div class="ml-action__compare__icon">
+                    <img src="{{asset('images/action/compare.svg')}}" alt="">
+                </div>
+            </div>
+            <div class="ml-action__like">
+                <div class="ml-action__like__icon">
+                    <img src="{{asset('images/action/like.svg')}}" alt="">
+                </div>
+            </div>
+            <div class="ml-action__cart">
+                <div class="ml-action__cart__icon">
+                    <img src="{{asset('images/action/cart.svg')}}" alt="">
+                </div>
+                <div class="ml-action__cart__text">Корзина</div>
+            </div>
+        </div>
     </div>
-
-
 </div>
+<nav class="main-menu">
+    <div class="container">
+    <div class="list-cat">
+        <div class="list-cat__main">
+            <div class="list-cat__main_icon">
+                <img src="{{asset('images/menu.svg')}}" alt="">
+            </div>
+            <span class="list-cat__main_text">Каталог</span>
+        </div>
+        <div class="list-cat__drop">
+        <ul class="list-cat__list">
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Комоды и Тумбы</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Мебель для детей</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Прихожие</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Стенки для гостиней</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Столы</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Шкафы</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Диваны</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Кресла</a></li>
+            <li class="list-cat__list-item"><a href="#" class="list-cat__list-link">Пуфы</a></li>
+        </ul>
+        </div>
+
+    </div>
+    <ul class="main-menu__list">
+            <li class="main-menu__item">
+                <a href="#" class="main-menu__link">Акции</a>
+            </li>
+            <li class="main-menu__item">
+                <a href="#" class="main-menu__link">О фабрике</a>
+            </li>
+            <li class="main-menu__item">
+                <a href="#" class="main-menu__link">Оплата</a>
+            </li>
+            <li class="main-menu__item">
+                <a href="#" class="main-menu__link">Произавдство</a>
+            </li>
+            <li class="main-menu__item">
+                <a href="#" class="main-menu__link">Сборки</a>
+            </li>
+            <li class="main-menu__item">
+                <a href="#" class="main-menu__link">Доставка</a>
+            </li>
+            <li class="main-menu__item">
+                <a href="#" class="main-menu__link">Оплата</a>
+            </li>
+    </ul>
+    </div>
+</nav>
+<header class="header">
+    <div class="container">
+
+    </div>
+</header>
 </body>
 </html>
